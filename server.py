9a22a565c5d73db99add76b4a8393ffdc2bd9508
Gunnar J. <GunnarJag@gmail.com>
2025-09-28 19:26:38 -0500

    note to self: review env syntax

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import db
 
 app = Flask(__name__)
-db.setup()
+setup()
 
 @app.route('/')
 @app.route('/<name>')
@@ -13,5 +13,67 @@ def hello(name=None):
 def submit():
     name = request.form.get("name")
     comment = request.form.get("comment")
-    db.add_post(name, comment)
+    add_post(name, comment)
     return render_template('hello.html', name="Visitor", guestbook=db.get_guestbook())
+
+#Taken from Kluver's demo repo. Currently in the "read/understand/edit" phase.
+
+from contextlib import contextmanager
+import logging
+import os
+
+from flask import current_app, g
+
+import psycopg2
+from psycopg2.pool import ThreadedConnectionPool
+from psycopg2.extras import DictCursor
+
+pool = None
+
+
+def setup():
+    global pool
+    DATABASE_URL = os.environ["DATABASE_URL"]
+    # current_app.logger.info(f"creating db connection pool")
+    pool = ThreadedConnectionPool(1, 100, dsn=DATABASE_URL, sslmode="require")
+
+
+@contextmanager
+def get_db_connection():
+    try:
+        connection = pool.getconn()
+        yield connection
+    finally:
+        pool.putconn(connection)
+
+
+@contextmanager
+def get_db_cursor(commit=False):
+    with get_db_connection() as connection:
+        cursor = connection.cursor(cursor_factory=DictCursor)
+        # cursor = connection.cursor()
+        try:
+            yield cursor
+            if commit:
+                connection.commit()
+        finally:
+            cursor.close()
+
+
+def add_post(name, text):
+    with get_db_cursor(True) as cur:
+        # current_app.logger.info("Adding guestbook post %s, %s", name, text)
+        cur.execute(
+            "INSERT INTO guest_book_entries(name, content) values (%s, %s);",
+            (name, text),
+        )
+
+
+def get_guestbook():
+    retval = []
+    with get_db_cursor(False) as cur:
+        with get_db_cursor() as cur:
+            cur.execute("select * from guests")
+            for row in cur:
+                retval.append({"name": row["name"], "text": row["content"]})
+    return retval
